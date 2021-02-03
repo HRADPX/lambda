b2d0724284e062bd6672a3a21a37d40aaf4f86c8
HR <ranhuang_5464@163.com>
2021-02-04 01:22:56 +0800

    1

---
+++ src/com/kuaishou/async/CompletableFutureTask.java
@@ -0,0 +1,48 @@
+package com.kuaishou.async;
+
+import com.kuaishou.model.DataInfo;
+import com.kuaishou.model.DbInfo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CompletableFutureTask {
+
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+    public static void main(String[] args) {
+
+        List<DbInfo> list = Arrays.asList(new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),new DbInfo(),
+                new DbInfo());
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        list.forEach(dataInfo -> {
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                try {
+                    System.out.println("start task...");
+                    long start = System.currentTimeMillis();
+                    Thread.sleep(new Random(200).nextInt(4) * 1000);
+                    dataInfo.setName("");
+                    dataInfo.setTaskName("TA");
+                    long end = System.currentTimeMillis();
+                    System.out.println("task spend time " + (end - start) / 1000 + " s");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, executorService);
+            futures.add(future);
+        });
+        System.out.println("async task all have submit, wait to end....");
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        System.out.println("async task all end....");
+        list.forEach(dbInfo -> {
+            System.out.println(dbInfo.getName().length());
+            System.out.println(dbInfo.getTaskName().replace("a","b"));
+        });
+    }
+}
---
+++ src/com/kuaishou/model/DbInfo.java
@@ -0,0 +1,24 @@
+package com.kuaishou.model;
+
+public class DbInfo {
+
+    private String name;
+
+    private String taskName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+}
